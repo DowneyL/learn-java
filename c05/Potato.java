ce27890fdfaf62d565377e4174fc018c31e5260e
aragakiyui <heng8050lee@foxmail.com>
2018-03-16 14:45:53 +0800

    day 10

---
+++ c05/Potato.java
@@ -0,0 +1,9 @@
+package c05;
+//:Potato.java
+
+public class Potato {
+	protected String c = "lala";
+	protected void f() {
+		System.out.println("f()");
+	}
+}///:~
---
+++ c05/Tomato.java
@@ -0,0 +1,10 @@
+package c05;
+//:Tomato.java
+
+public class Tomato {
+	public static void main(String[] args) {
+		Potato p = new Potato();
+		p.f();
+		System.out.println(p.c);
+	}
+}///:~
