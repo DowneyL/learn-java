d0f861a4f017761993d133b6cdf15bf5faf7b0d3
aragakiyui <heng8050lee@foxmail.com>
2018-03-24 14:00:20 +0800

    d16

---
+++ c07/innerscopes/Parcel9.java
@@ -0,0 +1,29 @@
+package c07.innerscopes;
+//:Parcel9.java
+// Using "instance initialization" to perform
+// constraction on an anoymous inner class
+
+public class Parcel9 {
+	public Destination dest(final String dest, final float price) {
+		return new Destination() {
+			private int cost;
+			// Instance initialization for each object:
+			{
+				cost = Math.round(price);
+				if(cost > 100) {
+					System.out.println("Over budget!");
+				}
+			}
+
+			private String label = dest;
+			public String readLabel() {
+				return label;
+			}
+		};
+	}
+	
+	public static void main(String[] args) {
+		Parcel9 p = new Parcel9();
+		Destination d = p.dest("Tanzania", 101.395F);
+	}
+}///:~
---
+++ c07/parcel11/Parcel11.java
@@ -0,0 +1,28 @@
+package c07.parcel11;
+//: Parcel11.java
+// Creating inner classes
+
+public class Parcel11 {
+	class Contents {
+		private int i = 11;
+		public int value() { return 1; }
+	}
+	
+	class Destination {
+		private String label;
+		Destination(String whereTo) {
+			label = whereTo;
+		}
+		String readLabel() {
+			return label;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Parcel11 p = new Parcel11();
+		// Must use instance of outer class
+		// to create an instances of the inner class:
+		Parcel11.Contents c = p.new Contents();
+		Parcel11.Destination d = p.new Destination("Tanzania");
+	}
+}
